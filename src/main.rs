4c46e2cc4cce8abf450483ad74375a35009d52a3
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-12-01 21:59:27 +0000

    Refactored in module

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
-use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    response::IntoResponse,
-    Json, Router};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use serde::{Deserialize, Serialize};
+
+mod controllers;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/health", get(health));
+    let app = Router::new().route("/health", get(controllers::health::get));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-#[derive(Deserialize, Serialize)]
-struct HealthResponse {
-    ok: bool
-}
-
-async fn health() -> impl IntoResponse {
-    let health_response = HealthResponse {
-        ok: true
-    };
-    (StatusCode::OK, Json(health_response))
 }
